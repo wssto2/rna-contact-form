96e096fe4f7bb784ee08ed1e7904df74cfb40374
jzlimen <josipzg28@gmail.com>
2021-07-13 13:13:04 +0200

    Rollup config

---
+++ build/rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
+import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+import image from '@rollup/plugin-image';
+
+export default {
+    input: 'src/wrapper.js', // Path relative to package.json
+    output: {
+        name: 'RnaContactForm',
+        exports: 'named',
+    },
+    plugins: [
+        commonjs(),
+        vue({
+            css: true, // Dynamically inject css as a <style> tag
+            compileTemplate: true, // Explicitly convert template to render function
+        }),
+        buble(), // Transpile to ES5,
+        image()
+    ],
+};
