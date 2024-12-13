8047642638b9a511942e3d4aaf20b78ea61bcc11
grgaborac <115068030+gborac@users.noreply.github.com>
2024-12-13 18:27:53 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
 module.exports = {
     mode: "production",
+    configureWebpack: config => {
+        config.optimization = {
+            concatenateModules: false,
+            providedExports: false,
+            usedExports: false
+        };
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            hash: 'sha512',
+                            digest: 'hex',
+                            name: '[hash].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            optimizationLevel: 7,
+                            interlaced: false
+                        }
+                    }
                 ]
             }
         ]
